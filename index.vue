a9d18d2b582f093d5d853a967f89fc45069f14ce
Jesse Smart <jsmartdev@protonmail.com>
2021-03-08 15:17:34 -0500

    Complete vue components

--- index.vue
+++ index.vue
@@ -16,3 +16,19 @@ let secondVue = new Vue({
         }
     }
 })
+
+Vue.component('grocery-item', {
+    props: ['groceryitem'],
+    template: '<li>{{ groceryitem.name }} {{ groceryitem.quantity}}</li>'
+})
+
+let thirdVue = new Vue({
+    el:'#thirdDiv',
+    data: {
+        groceryItems: [
+            {name: 'Bread', quantity: 2},
+            {name: 'Cheese', quantity: 3},
+            {name: 'Wine', quantity: 4}
+        ]
+    } 
+})
